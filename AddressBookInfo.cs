1a4bc826d644e8a9e5daea12aa584db2d9bbb9a9
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-02-22 00:43:46 +0530

    Update AddressBookInfo.cs

--- AddressBookInfo.cs
+++ AddressBookInfo.cs
@@ -1,1 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookInfo
+    {
+        public Contact[] ContactInfo { get; set; }
+        public int NumberOfContacts { get; set; }
+
+        public AddressBookInfo()
+        {
+            ContactInfo = new Contact[100];
+            NumberOfContacts = 0;
+        }
+
+        public void AddContact(Contact contact)
+        {
+            ContactInfo[NumberOfContacts] = contact;
+            NumberOfContacts++;
+        }
+
+
+        public void PrintContacts()
+        {
+            for (int i = 0; i < NumberOfContacts; i++)
+            {
+                Console.WriteLine("Name: {0} {1}\nCountry: {2}\nPhone: {3}\nEmail: {4}\n",
+                    ContactInfo[i].firstName, ContactInfo[i].lastName, ContactInfo[i].country, ContactInfo[i].phoneNumber, ContactInfo[i].email);
+            }
+        }
+    }
+}
 
